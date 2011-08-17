2b8197dbe084783eaf598712c3e5acde4810b5f4
Alex Soto <dxdream@gmail.com>
2011-08-17 17:18:54 -0500

    added missing code to be bound

--- admob/libGoogleAdMobAds.cs
+++ admob/libGoogleAdMobAds.cs
@@ -194,5 +194,44 @@ namespace libGoogleAdMobAds
 //	    NSString kGADErrorDomain { get; set; }
 //	}
 	
+	
+//	this is the only missing stuff not bound 
+//
+//
+//#import <Foundation/Foundation.h>
+//
+//@class GADRequest;
+//
+//extern NSString *kGADErrorDomain;
+//
+//// NSError codes for GAD error domain.
+//typedef enum {
+//  // The ad request is invalid.  The localizedFailureReason error description
+//  // will have more details.  Typically this is because the ad did not have the
+//  // ad unit ID or root view controller set.
+//  kGADErrorInvalidRequest,
+//
+//  // The ad request was successful, but no ad was returned.
+//  kGADErrorNoFill,
+//
+//  // There was an error loading data from the network.
+//  kGADErrorNetworkError,
+//
+//  // The ad server experienced a failure processing the request.
+//  kGADErrorServerError,
+//
+//  // The current device's OS is below the minimum required version.
+//  kGADErrorOSVersionTooLow,
+//
+//  // The request was unable to be loaded before being timed out.
+//  kGADErrorTimeout,
+//
+//} GADErrorCode;
+//
+//// This class represents the error generated due to invalid request parameters.
+//@interface GADRequestError : NSError
+//
+//@end
+	
 }
 
