760e3fabad3b977c06ecb17a5a709ee0bc16df7c
Alex Soto <dxdream@gmail.com>
2012-09-07 13:55:56 -0500

    Cleaned Project

--- Bindings/AlexTouch.GoogleAdMobAds/AlexTouch.GoogleAdMobAds/libGoogleAdMobAds.linkwith.cs
+++
@@ -1,4 +0,0 @@
-using System;
-using MonoTouch.ObjCRuntime;
-
-[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV6 | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics")]
