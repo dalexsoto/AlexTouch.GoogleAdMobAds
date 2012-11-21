038735421a785b4a1bd94ae36fed14f8c6eb17dc
jlyonsmith <john@lyon-smith.org>
2012-11-20 21:42:56 -0800

    Added reference to StoreKit to allow iPhone builds to link.
    Added a post build makefile to build documentation, but didn't link it in to .csproj.

--- AlexTouch.GoogleAdMobAds/libGoogleAdMobAds.linkwith.cs
+++ AlexTouch.GoogleAdMobAds/libGoogleAdMobAds.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics MediaPlayer", WeakFrameworks = "AdSupport", IsCxx = true, LinkerFlags = "-lz -lsqlite3")]
+[assembly: LinkWith ("libGoogleAdMobAds.a", LinkTarget.Simulator | LinkTarget.ArmV7, ForceLoad = true, Frameworks = "AudioToolbox MessageUI SystemConfiguration CoreGraphics MediaPlayer StoreKit", WeakFrameworks = "AdSupport", IsCxx = true, LinkerFlags = "-lz -lsqlite3")]
